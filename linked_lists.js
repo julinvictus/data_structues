a59c306c08556dc59f2d562a2ec1d6e405878814
Juliana Almeida <juliana.almeida@remix.com>
2020-07-06 20:39:08 -0700

    Add delete to LL plus py files

--- linked_lists.js
+++ linked_lists.js
@@ -1,83 +1,100 @@
+// run file:  node linked_lists.js
+
 class LinkedList {
-    constructor(value) {
-        this.head = null;
-        this.length = 0;
-        this.addToHead(value);
+  constructor(value) {
+    this.head = null;
+    this.length = 0;
+    this.addToHead(value);
+  }
+  // add items to the head
+  addToHead(value) {
+    return this.addToSpecificIndex(0, value);
+    // const newNode = { value }; // 1
+    // // next is pointing to whatever the head is pointing at:
+    // newNode.next = this.head;  // 2
+    // this.head = newNode;       // 3
+    // this.length++;
+    // return this;
+  }
+  addToTail(value) {
+    return this.addToSpecificIndex(this.length, value);
+    // const newNode = { value };  //a
+    // if (this.head == null) {
+    //     this.head = newNode;
+    // }
+    // else {
+    //     let currentNode = this.head
+    //     // while currentNode.next is not null
+    //     while (currentNode.next){
+    //         // hop from node to node
+    //         currentNode = currentNode.next;
+    //     }
+    //     // end of the list
+    //     currentNode.next = newNode
+    // }
+    // this.length++;
+    // return this;
+  }
+  addToSpecificIndex(index, value) {
+    const newNode = { value };
+    let currentNode = this.head;
+    let previousNode;
+    let currentIndex = 0;
+    if (index === 0) {
+      this.head = newNode;
+      newNode.next = currentNode;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = newNode;
+      newNode.next = currentNode;
     }
-    // add items to the head
-    addToHead(value) {
-        return this.addToSpecificIndex(0, value);
-        // const newNode = { value }; // 1
-        // // next is pointing to whatever the head is pointing at:
-        // newNode.next = this.head;  // 2 
-        // this.head = newNode;       // 3
-        // this.length++;
-        // return this;
+    this.length++;
+    // return currentNode.value
+    return this;
+  }
+  delete(value) {
+    const nodeToBeDeleted = { value };
+    let currentNode = this.head;
+    let previousNode;
+    // console.log('to be deleted' + JSON.stringify(nodeToBeDeleted));
+    // console.log('current' + JSON.stringify(currentNode));
+    while (currentNode.value !== nodeToBeDeleted.value && currentNode.next) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
     }
-    addToTail(value){
-        return this.addToSpecificIndex(this.length, value);
-        // const newNode = { value };  //a
-        // if (this.head == null) {
-        //     this.head = newNode;
-        // } 
-        // else {
-        //     let currentNode = this.head
-        //     // while currentNode.next is not null
-        //     while (currentNode.next){ 
-        //         // hop from node to node
-        //         currentNode = currentNode.next; 
-        //     }
-        //     // end of the list
-        //     currentNode.next = newNode
-        // }
-        // this.length++;
-        // return this;
+    if (currentNode.value === nodeToBeDeleted.value) {
+      if (previousNode) {
+        previousNode.next = currentNode.next;
+        this.length--;
+      } else {
+        this.head = currentNode.next;
+        this.length--;
+      }
     }
-    addToSpecificIndex (index, value){
-        const newNode = { value }; 
-        let currentNode = this.head;
-        let previousNode;
-        let currentIndex = 0;
-        if (index === 0) {
-            this.head = newNode;
-            newNode.next = currentNode;
-        } else {
-            while (currentIndex < index){
-                currentIndex++;
-                previousNode = currentNode;
-                currentNode = currentNode.next;
-            }
-            previousNode.next = newNode;
-            newNode.next = currentNode;
-            
-        }
-        this.length++;
-        // return currentNode.value
-        return this;
-    }
+  }
 }
-// const list = new LinkedList('first')
-//     .addToHead('second')
-//     .addToHead('third');
+// const list = new LinkedList('first').addToHead('second');
+// .addToHead('third');
 
-// const letters = new LinkedList('a')
-//     .addToTail('b')
-//     .addToTail('c')
+// const letters = new LinkedList('a').addToTail('b').addToTail('c');
 
-// const myName = new LinkedList('j')
-//     .addToTail('u')
-//     .addToTail('i')
-//     .addToSpecificIndex(2, 'l')
+// const myName = new LinkedList('j').addToTail('u').addToTail('i').addToSpecificIndex(2, 'l');
 
-//const singleElementList = new LinkedList('oi')
+// const singleElementList = new LinkedList('oi');
 
-// console.log("Adding to the head:")
+// console.log('Add 1st 2nd 3rd to the head then remove 3rd:');
+// list.delete('second');
 // console.log(JSON.stringify(list, null, 2));
-// console.log("-------------------")
-// console.log("Adding to the tail:")
-// console.log(JSON.stringify(letters, null, 2));
-// console.log("-------------------")
-// console.log("Adding to specific index:")
+// console.log('-------------------');
+// console.log('Add a b c to the tail then delete c:');
+// letters.delete('c');
+// console.log(JSON.stringify(letters));
+// console.log('-------------------');
+// console.log('Adding to specific index:');
 // console.log(JSON.stringify(myName, null, 2));
 
 //console.log(singleElementList)
--- linked_lists.test.js
+++ linked_lists.test.js
@@ -1,66 +1,71 @@
+// run file: npm run test
+
 const LinkedList = require('./linked_lists');
 
 test('it constructs a linked list', () => {
-  var singleElementList = new LinkedList('oi')
-  expect(singleElementList).toEqual(
-    { head: { next: null, value: 'oi' }, length: 1 }
-  )
-})
+  var singleElementList = new LinkedList('oi');
+  expect(singleElementList).toEqual({ head: { next: null, value: 'oi' }, length: 1 });
+});
 
 test('adds node to head', () => {
-  const list = new LinkedList('first')
-  expect(list.addToHead('second')).toEqual(
-    {
-      head: {
-        value: 'second',
-        next: {
-          value: 'first',
-          next: null,
-        },
+  const list = new LinkedList('first');
+  expect(list.addToHead('second')).toEqual({
+    head: {
+      value: 'second',
+      next: {
+        value: 'first',
+        next: null,
       },
-      length: 2,
-    }
-  );
+    },
+    length: 2,
+  });
 });
 
 test('adds node to tail', () => {
-  const letters = new LinkedList('a')
-  expect(letters.addToTail('b')).toEqual(
-    {
-      head: {
-        value: 'a',
-        next: {
-          value: 'b',
-          next: null,
-        },
+  const letters = new LinkedList('a');
+  expect(letters.addToTail('b')).toEqual({
+    head: {
+      value: 'a',
+      next: {
+        value: 'b',
+        next: null,
       },
-      length: 2,
-    }
-  );
+    },
+    length: 2,
+  });
 });
 
 test('adds node to specific index', () => {
-  const myName = new LinkedList('j')
-  expect(myName
-    .addToTail('u')
-    .addToTail('i')
-    .addToSpecificIndex(2, 'l'),
-  ).toEqual(
-    {
-      head: {
-        value: 'j',
+  const myName = new LinkedList('j');
+  expect(myName.addToTail('u').addToTail('i').addToSpecificIndex(2, 'l')).toEqual({
+    head: {
+      value: 'j',
+      next: {
+        value: 'u',
         next: {
-          value: 'u',
+          value: 'l',
           next: {
-            value: 'l',
-            next: {
-              value: 'i',
-              next: null,
-            },
+            value: 'i',
+            next: null,
           },
         },
       },
-      length: 4,
-    }
-  );
+    },
+    length: 4,
+  });
+});
+
+test('deletes node with a specific value', () => {
+  const otherList = new LinkedList(1).addToHead(2).addToHead(3);
+  otherList.delete(3);
+  expect(otherList).toEqual({
+    head: {
+      value: 2,
+      next: {
+        value: 1,
+        next: null,
+      },
+    },
+    length: 2,
+  });
 });
